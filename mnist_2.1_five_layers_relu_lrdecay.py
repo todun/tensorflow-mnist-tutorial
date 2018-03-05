8b50ae722be515ed3ffd7a21ef191ee5dac2fd3e
todun <todun@users.noreply.github.com>
2018-03-05 13:28:34 +0100

    added docs to mnist v2.1

--- mnist_2.1_five_layers_relu_lrdecay.py
+++ mnist_2.1_five_layers_relu_lrdecay.py
@@ -17,6 +17,7 @@ import tensorflow as tf
 import tensorflowvisu
 import math
 from tensorflow.examples.tutorials.mnist import input_data as mnist_data
+
 print("Tensorflow version " + tf.__version__)
 tf.set_random_seed(0)
 
@@ -35,14 +36,14 @@ tf.set_random_seed(0)
 #          ·                                                        Y5 [batch, 10]
 
 # Download images and labels into mnist.test (10K images+labels) and mnist.train (60K images+labels)
-mnist = mnist_data.read_data_sets("data", one_hot=True, reshape=False, validation_size=0)
+mnist = mnist_data.read_data_sets(train_dir="data", one_hot=True, reshape=False, validation_size=0)
 
 # input X: 28x28 grayscale images, the first dimension (None) will index the images in the mini-batch
-X = tf.placeholder(tf.float32, [None, 28, 28, 1])
+X = tf.placeholder(dtype=tf.float32, shape=[None, 28, 28, 1])
 # correct answers will go here
-Y_ = tf.placeholder(tf.float32, [None, 10])
+Y_ = tf.placeholder(dtype=tf.float32, shape=[None, 10])
 # variable learning rate
-lr = tf.placeholder(tf.float32)
+lr = tf.placeholder(dtype=tf.float32)
 
 # five layers and their number of neurons (tha last layer has 10 softmax neurons)
 L = 200
@@ -51,83 +52,90 @@ N = 60
 O = 30
 # Weights initialised with small random values between -0.2 and +0.2
 # When using RELUs, make sure biases are initialised with small *positive* values for example 0.1 = tf.ones([K])/10
-W1 = tf.Variable(tf.truncated_normal([784, L], stddev=0.1))  # 784 = 28 * 28
-B1 = tf.Variable(tf.ones([L])/10)
-W2 = tf.Variable(tf.truncated_normal([L, M], stddev=0.1))
-B2 = tf.Variable(tf.ones([M])/10)
-W3 = tf.Variable(tf.truncated_normal([M, N], stddev=0.1))
-B3 = tf.Variable(tf.ones([N])/10)
-W4 = tf.Variable(tf.truncated_normal([N, O], stddev=0.1))
-B4 = tf.Variable(tf.ones([O])/10)
-W5 = tf.Variable(tf.truncated_normal([O, 10], stddev=0.1))
-B5 = tf.Variable(tf.zeros([10]))
+W1 = tf.Variable(initial_value=tf.truncated_normal(shape=[784, L], stddev=0.1))  # 784 = 28 * 28
+B1 = tf.Variable(initial_value=tf.ones([L]) / 10)
+W2 = tf.Variable(initial_value=tf.truncated_normal(shape=[L, M], stddev=0.1))
+B2 = tf.Variable(initial_value=(tf.ones(shape=[M]) / 10))
+W3 = tf.Variable(initial_value=tf.truncated_normal(shape=[M, N], stddev=0.1))
+B3 = tf.Variable(initial_value=tf.ones([N]) / 10)
+W4 = tf.Variable(initial_value=tf.truncated_normal([N, O], stddev=0.1))
+B4 = tf.Variable(initial_value=tf.ones(shape=[O]) / 10)
+W5 = tf.Variable(initial_value=tf.truncated_normal(shape=[O, 10], stddev=0.1))
+B5 = tf.Variable(initial_value=tf.zeros(shape=[10]))
 
 # The model
-XX = tf.reshape(X, [-1, 784])
-Y1 = tf.nn.relu(tf.matmul(XX, W1) + B1)
-Y2 = tf.nn.relu(tf.matmul(Y1, W2) + B2)
-Y3 = tf.nn.relu(tf.matmul(Y2, W3) + B3)
-Y4 = tf.nn.relu(tf.matmul(Y3, W4) + B4)
-Ylogits = tf.matmul(Y4, W5) + B5
-Y = tf.nn.softmax(Ylogits)
+XX = tf.reshape(tensor=X, shape=[-1, 784])
+Y1 = tf.nn.relu(features=tf.matmul(a=XX, b=W1) + B1)
+Y2 = tf.nn.relu(features=tf.matmul(a=Y1, b=W2) + B2)
+Y3 = tf.nn.relu(features=tf.matmul(a=Y2, b=W3) + B3)
+Y4 = tf.nn.relu(features=tf.matmul(a=Y3, b=W4) + B4)
+Ylogits = tf.matmul(a=Y4, b=W5) + B5
+Y = tf.nn.softmax(logits=Ylogits)
 
 # cross-entropy loss function (= -sum(Y_i * log(Yi)) ), normalised for batches of 100  images
 # TensorFlow provides the softmax_cross_entropy_with_logits function to avoid numerical stability
 # problems with log(0) which is NaN
 cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits=Ylogits, labels=Y_)
-cross_entropy = tf.reduce_mean(cross_entropy)*100
+cross_entropy = tf.reduce_mean(input_tensor=cross_entropy) * 100
 
 # accuracy of the trained model, between 0 (worst) and 1 (best)
-correct_prediction = tf.equal(tf.argmax(Y, 1), tf.argmax(Y_, 1))
+correct_prediction = tf.equal(x=tf.argmax(input=Y, axis=1), y=tf.argmax(input=Y_, axis=1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
 # matplotlib visualisation
-allweights = tf.concat([tf.reshape(W1, [-1]), tf.reshape(W2, [-1]), tf.reshape(W3, [-1]), tf.reshape(W4, [-1]), tf.reshape(W5, [-1])], 0)
-allbiases  = tf.concat([tf.reshape(B1, [-1]), tf.reshape(B2, [-1]), tf.reshape(B3, [-1]), tf.reshape(B4, [-1]), tf.reshape(B5, [-1])], 0)
-I = tensorflowvisu.tf_format_mnist_images(X, Y, Y_)
-It = tensorflowvisu.tf_format_mnist_images(X, Y, Y_, 1000, lines=25)
+allweights = tf.concat(values=[tf.reshape(W1, [-1]), tf.reshape(W2, [-1]), tf.reshape(W3, [-1]), tf.reshape(W4, [-1]),
+                               tf.reshape(W5, [-1])], axis=0)
+allbiases = tf.concat(values=[tf.reshape(B1, [-1]), tf.reshape(B2, [-1]), tf.reshape(B3, [-1]), tf.reshape(B4, [-1]),
+                              tf.reshape(B5, [-1])], axis=0)
+I = tensorflowvisu.tf_format_mnist_images(X=X, Y=Y, Y_=Y_)
+It = tensorflowvisu.tf_format_mnist_images(X=X, Y=Y, Y_=Y_, n=1000, lines=25)
 datavis = tensorflowvisu.MnistDataVis()
 
 # training step, the learning rate is a placeholder
-train_step = tf.train.AdamOptimizer(lr).minimize(cross_entropy)
+train_step = tf.train.AdamOptimizer(learning_rate=lr).minimize(loss=cross_entropy)
 
 # init
 init = tf.global_variables_initializer()
 sess = tf.Session()
-sess.run(init)
+sess.run(fetches=init)
 
 
 # You can call this function in a loop to train the model, 100 images at a time
 def training_step(i, update_test_data, update_train_data):
-
     # training on batches of 100 images with 100 labels
-    batch_X, batch_Y = mnist.train.next_batch(100)
+    batch_X, batch_Y = mnist.train.next_batch(batch_size=100)
 
     # learning rate decay
     max_learning_rate = 0.003
     min_learning_rate = 0.0001
-    decay_speed = 2000.0 # 0.003-0.0001-2000=>0.9826 done in 5000 iterations
-    learning_rate = min_learning_rate + (max_learning_rate - min_learning_rate) * math.exp(-i/decay_speed)
+    decay_speed = 2000.0  # 0.003-0.0001-2000=>0.9826 done in 5000 iterations
+    learning_rate = min_learning_rate + (max_learning_rate - min_learning_rate) * math.exp(-i / decay_speed)
 
     # compute training values for visualisation
     if update_train_data:
-        a, c, im, w, b = sess.run([accuracy, cross_entropy, I, allweights, allbiases], {X: batch_X, Y_: batch_Y})
+        a, c, im, w, b = sess.run(fetches=[accuracy, cross_entropy, I, allweights, allbiases],
+                                  feed_dict={X: batch_X, Y_: batch_Y})
         print(str(i) + ": accuracy:" + str(a) + " loss: " + str(c) + " (lr:" + str(learning_rate) + ")")
-        datavis.append_training_curves_data(i, a, c)
-        datavis.update_image1(im)
-        datavis.append_data_histograms(i, w, b)
+        datavis.append_training_curves_data(x=i, accuracy=a, loss=c)
+        datavis.update_image1(im=im)
+        datavis.append_data_histograms(x=i, datavect1=w, datavect2=b)
 
     # compute test values for visualisation
     if update_test_data:
-        a, c, im = sess.run([accuracy, cross_entropy, It], {X: mnist.test.images, Y_: mnist.test.labels})
-        print(str(i) + ": ********* epoch " + str(i*100//mnist.train.images.shape[0]+1) + " ********* test accuracy:" + str(a) + " test loss: " + str(c))
-        datavis.append_test_curves_data(i, a, c)
-        datavis.update_image2(im)
+        a, c, im = sess.run(fetches=[accuracy, cross_entropy, It],
+                            feed_dict={X: mnist.test.images, Y_: mnist.test.labels})
+        print(str(i) + ": ********* epoch " + str(
+            i * 100 // mnist.train.images.shape[0] + 1) + " ********* test accuracy:" + str(a) + " test loss: " + str(
+            c))
+        datavis.append_test_curves_data(x=i, accuracy=a, loss=c)
+        datavis.update_image2(im=im)
 
     # the backpropagation training step
-    sess.run(train_step, {X: batch_X, Y_: batch_Y, lr: learning_rate})
+    sess.run(fetches=train_step, feed_dict={X: batch_X, Y_: batch_Y, lr: learning_rate})
 
-datavis.animate(training_step, iterations=10000+1, train_data_update_freq=20, test_data_update_freq=100, more_tests_at_start=True)
+
+datavis.animate(compute_step=training_step, iterations=10000 + 1, train_data_update_freq=20, test_data_update_freq=100,
+                more_tests_at_start=True)
 
 # to save the animation as a movie, add save_movie=True as an argument to datavis.animate
 # to disable the visualisation use the following line instead of the datavis.animate line
